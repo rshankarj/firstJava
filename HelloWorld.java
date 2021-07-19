f09095a58b1c56b826f8647dc28d355153e91725
rshankarj <rshankarj@gmail.com>
2021-07-19 17:17:09 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld  {
 public static void main(String arg[]) {
-System.out.println("Hello world");
+System.out.println("Hello world welcome to Jenkins testing environment");
 }
 }
